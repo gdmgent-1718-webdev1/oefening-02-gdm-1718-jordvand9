87e6c2b671795dcbf42c9b95d886304ef5c9ebaf
gdm-1718-jordvand9 <jordvand9@student.arteveldehs.be>
2017-10-05 10:35:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$string1 = 'Hello';
+$string2 = 'World';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oefening 1</title>
+</head>
+<body>
+    <p><?= $string1 . " " . $string2 ?></p>
+</body>
+</html>
